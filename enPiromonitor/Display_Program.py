67d6421ded74d4cab34516fb5bec57673a8e25e5
arstgkneio <andreygorinov@gmail.com>
2020-04-18 03:46:13 +0100

    add update_bg_color(PM_type)

--- enPiromonitor/Display_Program.py
+++ enPiromonitor/Display_Program.py
@@ -5,16 +5,18 @@ import glob
 
 # Settings
 monitor_resolution = '1024x600'
+fullscreen_boolean = 'True'
 
 # Retrieve the latest data file
 path = '/home/pi/Desktop/Air_Quality_Monitor_Code'
 file_prefix = 'environment_data'
 
+
 # Function to retrieve the last line from a file
 def get_last_line():
     file_list = glob.glob(path + '/' + file_prefix + '*.csv')
     file_list.sort()
-    datafile = file_list[-1]
+    datafile = file_list[-1] #most recent data file
     
     with open(datafile, 'rb') as f:
         f.seek(-2, os.SEEK_END)
@@ -26,11 +28,134 @@ def get_last_line():
 # Padding character
 padding_char = '_'
 
+# Function to update background color variable based on air quality index
+def update_bg_color(PM_type):
+    raw_data_line = get_last_line().rstrip()
+    aqi_2_5 = raw_data_line.split(',')[2]
+    aqi_10 = raw_data_line.split(',')[4]
+
+    aqi_color = 'black'
+    aqi_color_2_5 = 'black'
+    aqi_color_10 = 'black'
+    
+    if float(aqi_2_5) >= 200:
+        aqi_color_2_5 = 'maroon'
+    elif float(aqi_2_5) >= 150:
+        aqi_color_2_5 = 'red'
+    elif float(aqi_2_5) >= 100:
+        aqi_color_2_5 = 'orange'
+    elif float(aqi_2_5) >= 50:
+        aqi_color_2_5 = 'yellow'
+    elif float(aqi_2_5) >= 0:
+        aqi_color_2_5 = 'green'
+    else:
+        aqi_color_2_5 = 'black'
+        
+    if float(aqi_10) >= 200:
+        aqi_color_10 = 'maroon'
+    elif float(aqi_10) >= 150:
+        aqi_color_10 = 'red'
+    elif float(aqi_10) >= 100:
+        aqi_color_10 = 'orange'
+    elif float(aqi_10) >= 50:
+        aqi_color_10 = 'yellow'
+    elif float(aqi_10) >= 0:
+        aqi_color_10 = 'green'
+    else:
+        aqi_color_10 = 'black'
+        
+    #print(aqi_color)
+    if PM_type == '2_5':
+        return(aqi_color_2_5)
+    elif PM_type == '10':
+        return(aqi_color_10)
+    else:
+        return(aqi_color)
+'''
+    if float(aqi_2_5) >= 200 or float(aqi_10) >= 200:
+        aqi_color = 'maroon'
+    elif float(aqi_2_5) >= 150 or float(aqi_10) >= 150:
+        aqi_color = 'red'
+    elif float(aqi_2_5) >= 100 or float(aqi_10) >= 100:
+        aqi_color = 'orange'
+    elif float(aqi_2_5) >= 50 or float(aqi_10) >= 50:
+        aqi_color = 'yellow'
+    elif float(aqi_2_5) >= 0 or float(aqi_10) >= 0:
+        aqi_color = 'green'
+    else:
+        aqi_color = 'black'
+'''
+
+
+
+# Function which updates background of static labels
+#def update_bg_of_static_label(label_name):
+#    label_name['bg'] = update_bg_color()
+#   
+#    label_name.after(5000, update_bg_of_static_label)
+
+def update_my_window_label():
+    my_window['bg'] = update_bg_color('0')
+    
+    my_window.after(5000, update_my_window_label)
+
+def update_title_label():
+    title_label['bg'] = update_bg_color('0')
+    
+    title_label.after(5000, update_title_label)
+
+def update_clock_label():
+    clock_label['bg'] = update_bg_color('0')
+    
+    clock_label.after(5000, update_clock_label)
+
+def update_temp_title_label():
+    temp_title_label['bg'] = update_bg_color('0')
+    
+    temp_title_label.after(5000, update_temp_title_label)
+
+def update_temp_unit_label():
+    temp_unit_label['bg'] = update_bg_color('0')
+    
+    temp_unit_label.after(5000, update_temp_unit_label)
+
+def update_press_title_label():
+    press_title_label['bg'] = update_bg_color('0')
+    
+    press_title_label.after(5000, update_press_title_label)
+
+def update_press_unit_label():
+    press_unit_label['bg'] = update_bg_color('0')
+    
+    press_unit_label.after(5000, update_press_unit_label)
+
+def update_humid_title_label():
+    humid_title_label['bg'] = update_bg_color('0')
+    
+    humid_title_label.after(5000, update_humid_title_label)
+
+def update_humid_unit_label():
+    humid_unit_label['bg'] = update_bg_color('0')
+    
+    humid_unit_label.after(5000, update_humid_unit_label)
+
+def update_aqi_2_5_title_label():
+    aqi_2_5_title_label['bg'] = update_bg_color('2_5')
+    
+    aqi_2_5_title_label.after(5000, update_aqi_2_5_title_label)
+
+def update_aqi_10_title_label():
+    aqi_10_title_label['bg'] = update_bg_color('10')
+    
+    aqi_10_title_label.after(5000, update_aqi_10_title_label)
+
 # Function to update temp_data_label with latest reading from a file
 def update_temp_data_label():
     raw_data_line = get_last_line().rstrip()
     temperature = raw_data_line.split(',')[5]
     temp_data_label['text'] = "{:6.1f}".format(float(temperature))
+
+    temp_data_label['bg'] = update_bg_color('0')
     
     temp_data_label.after(5000, update_temp_data_label)
 
@@ -39,6 +164,8 @@ def update_press_data_label():
     raw_data_line = get_last_line().rstrip()
     pressure = raw_data_line.split(',')[8]
     press_data_label['text'] = "{:6.1f}".format(float(pressure))
+
+    press_data_label['bg'] = update_bg_color('0')
     
     press_data_label.after(5000, update_press_data_label)
 
@@ -47,15 +174,21 @@ def update_humid_data_label():
     raw_data_line = get_last_line().rstrip()
     humidity = raw_data_line.split(',')[7]
     humid_data_label['text'] = "{:6.1f}".format(float(humidity))
+
+    humid_data_label['bg'] = update_bg_color('0')
     
     humid_data_label.after(5000, update_humid_data_label)
-    
 
 # Function to update aqi_2_5_data_label with latest reading from a file
 def update_aqi_2_5_data_label():
     raw_data_line = get_last_line().rstrip()
     aqi_2_5 = raw_data_line.split(',')[2]
     aqi_2_5_data_label['text'] = "{:6.1f}".format(float(aqi_2_5))
+
+    aqi_2_5_data_label['bg'] = update_bg_color('2_5')
+    
+
+   # aqi_2_5_data_label.configure(bg=aqi_color) #updates background color in a different way
     
     aqi_2_5_data_label.after(5000, update_aqi_2_5_data_label)
     
@@ -65,6 +198,10 @@ def update_aqi_10_data_label():
     aqi_10 = raw_data_line.split(',')[4]
     aqi_10_data_label['text'] = "{:6.1f}".format(float(aqi_10))
     
+    aqi_10_data_label['bg'] = update_bg_color('10')
+
+    #aqi_10_data_label.configure(bg=aqi_color)
+
     aqi_10_data_label.after(5000, update_aqi_10_data_label)
 
 # Function to update clock_label widget with system clock reading
@@ -73,91 +210,100 @@ def display_time():
     clock_label['text'] = current_time
     clock_label.after(200, display_time)
 
+
+
+#update_bg_color()
 # Create the main window with and set its atributes
 my_window = tk.Tk()
 my_window.title('Environment Data')
 #my_window.configure(bg='black')
-my_window['bg']='black' # has same effect as the line above
+my_window['bg']=update_bg_color('0') # has same effect as the line above
+#update_my_window_label()
 
 my_window.geometry(monitor_resolution)
 #my_window.overrideredirect(True)
-#my_window.wm_attributes('-fullscreen','true')
+my_window.wm_attributes('-fullscreen',fullscreen_boolean)
 #my_window.wm_attributes('-topmost','true')
 
 # Create the title_label widget
-clock_label = tk.Label(my_window, text='EnPIronment Monitor', font='ariel 70', bg='black', fg='white')
-clock_label.grid(row=0, column=0, columnspan=2)
-
+title_label = tk.Label(my_window, text='En-pi-ronment Monitor', font='ariel 70', fg='white')
+title_label.grid(row=0, column=0, columnspan=2)
+update_title_label()
 
 # Create the clock_label widget
-clock_label = tk.Label(my_window, font='ariel 70', bg='black', fg='gray')
+clock_label = tk.Label(my_window, font='ariel 70', fg='gray')
 clock_label.grid(row=100, column=0, columnspan=3)
 display_time()
+update_clock_label()
 
 
 # Create the temp_title_label widget
-temp_title_label = tk.Label(my_window, text='Temperature'.ljust(23,padding_char), font='courier 45', bg='black', fg='gray')
+temp_title_label = tk.Label(my_window, text='Temperature'.ljust(23,padding_char), font='courier 45', fg='gray')
 temp_title_label.grid(row=1, column=0, sticky='W')
-
+update_temp_title_label()
 
 # Create the temp_data_label widget
-temp_data_label = tk.Label(my_window, font='courier 45 bold', bg='black', fg='gray')
+temp_data_label = tk.Label(my_window, font='courier 45 bold', fg='gray')
 temp_data_label.grid(row=1, column=1)
 update_temp_data_label()
 
 # Create the temp_unit_label widget
 degree_sign = u'\N{DEGREE SIGN}' #unicode
-temp_unit_label = tk.Label(my_window, text=degree_sign+'C', font='courier 45', bg='black', fg='gray')
+temp_unit_label = tk.Label(my_window, text=degree_sign+'C', font='courier 45', fg='gray')
 temp_unit_label.grid(row=1, column=2, sticky='W')
+update_temp_unit_label()
 
 # Create the press_title_label widget
-press_title_label = tk.Label(my_window, text='Pressure'.ljust(23,padding_char), font='courier 45', bg='black', fg='gray')
+press_title_label = tk.Label(my_window, text='Pressure'.ljust(23,padding_char), font='courier 45', fg='gray')
 press_title_label.grid(row=2, column=0, sticky='W')
-
+update_press_title_label()
 
 # Create the pressure_data_label widget
-press_data_label = tk.Label(my_window, font='courier 45 bold', bg='black', fg='gray')
+press_data_label = tk.Label(my_window, font='courier 45 bold', fg='gray')
 press_data_label.grid(row=2, column=1)
 update_press_data_label()
 
 # Create the press_unit_label widget
-press_unit_label = tk.Label(my_window, text='hPa', font='courier 45', bg='black', fg='gray')
+press_unit_label = tk.Label(my_window, text='hPa', font='courier 45', fg='gray')
 press_unit_label.grid(row=2, column=2, sticky='W')
+update_press_unit_label()
 
 # Create the humid_title_label widget
-humid_title_label = tk.Label(my_window, text='Humidity'.ljust(23,padding_char), font='courier 45', bg='black', fg='gray')
+humid_title_label = tk.Label(my_window, text='Humidity'.ljust(23,padding_char), font='courier 45', fg='gray')
 humid_title_label.grid(row=3, column=0, sticky='W')
+update_humid_title_label()
 
 
 # Create the humid_data_label widget
-humid_data_label = tk.Label(my_window, font='courier 45 bold', bg='black', fg='gray')
+humid_data_label = tk.Label(my_window, font='courier 45 bold', fg='gray')
 humid_data_label.grid(row=3, column=1)
 update_humid_data_label()
 
 
 # Create the humid_unit_label widget
-humid_unit_label = tk.Label(my_window, text='%', font='courier 45', bg='black', fg='gray')
+humid_unit_label = tk.Label(my_window, text='%', font='courier 45', fg='gray')
 humid_unit_label.grid(row=3, column=2, sticky='W')
+update_humid_unit_label()
 
 
 # Create the aqi_2_5_title_label widget
-aqi_2_5_title_label = tk.Label(my_window, text='AQI Index(2.5μm)'.ljust(23,padding_char), font='courier 45', bg='black', fg='gray')
+aqi_2_5_title_label = tk.Label(my_window, text='AQI Index(2.5μm)'.ljust(23,padding_char), font='courier 45', fg='gray')
 aqi_2_5_title_label.grid(row=4, column=0, sticky='W')
-
+update_aqi_2_5_title_label()
 
 # Create the aqi_2_5_data_label widget
-aqi_2_5_data_label = tk.Label(my_window, font='courier 45 bold', bg='black', fg='gray')
+aqi_2_5_data_label = tk.Label(my_window, font='courier 45 bold', fg='gray')
 aqi_2_5_data_label.grid(row=4, column=1)
 update_aqi_2_5_data_label()
 
 
 # Create the aqi_10_label widget
-aqi_10_title_label = tk.Label(my_window, text='AQI Index(10μm)'.ljust(23,padding_char), font='courier 45', bg='black', fg='gray')
+aqi_10_title_label = tk.Label(my_window, text='AQI Index(10μm)'.ljust(23,padding_char), font='courier 45', fg='gray')
 aqi_10_title_label.grid(row=5, column=0, sticky='W')
-
+update_aqi_10_title_label()
 
 # Create the aqi_10_data_label widget
-aqi_10_data_label = tk.Label(my_window, font='courier 45 bold', bg='black', fg='gray')
+aqi_10_data_label = tk.Label(my_window, font='courier 45 bold', fg='gray')
 aqi_10_data_label.grid(row=5, column=1)
 update_aqi_10_data_label()
 
@@ -165,3 +311,4 @@ update_aqi_10_data_label()
 
 # Initiate the window's main loop that waits for user's actions
 my_window.mainloop()
+
--- enPiromonitor/enPiromonitor.sh
+++ enPiromonitor/enPiromonitor.sh
@@ -8,3 +8,7 @@ python3 /<PATH_GOES_HERE>/Monitor_Program.py &
 
 
 python3 /<PATH_GOES_HERE>/Display_Program.py &
+
+
+# to run on boot with crontab add "@reboot sleep 30 && <PATH_GOES_HERE>/enPiromonitor.sh 2><PATH_GOES_HERE>/error_log.txt &"
+# see "running_python_programs_on_start_up.txt" for more information
